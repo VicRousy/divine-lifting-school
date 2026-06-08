3cf41c4c1baebdcd8ad56f85655127289154f3d8
You <you@example.com>
2026-06-08 16:38:38 +0100

    Remove 4 dead dependencies: canvg, autoprefixer, postcss, tailwindcss

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    include: ['jspdf', 'jspdf-autotable', 'canvg'],
+    include: ['jspdf', 'jspdf-autotable'],
   },
 })
